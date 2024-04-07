2149271aca39ace35d06f90aac5a8f6c6646daf2
Anton Burmagin <burmagin.anton.s@gmail.com>
2024-04-07 21:44:41 +0300

    second commit: solve 5th task

--- main.cpp
+++ main.cpp
@@ -19,8 +19,56 @@ bool gameFinished (char field[][3]) {
     return false;
 }
 
+void initField (bool field[][12]) {
+    for (int i = 0; i < 12; i++) {
+        for (int k = 0; k < 12; k++) {
+            field[i][k] = true;
+        }
+    }
+}
+
+bool fieldPopped (bool field[][12]) {
+    for (int i = 0; i < 12; i++) {
+        for (int k = 0; k < 12; k++) {
+            if (field[i][k])
+                return false;
+        }
+    }
+    return true;
+}
+
+void showField (bool field[][12]) {
+    for (int i = 0; i < 12; i++) {
+        for (int k = 0; k < 12; k++) {
+            if (field[i][k]) {
+                std::cout << "o";
+            } else {
+                std::cout << "x";
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+void popping (bool field[][12], int x1, int y1, int x2, int y2) {
+    if (x1 > x2)
+        std::swap(x1, x2);
+    if (y1 > y2)
+        std::swap(y1, y2);
+    for (int i = x1; i <= x2; i++) {
+        for (int k = y1; k <= y2; k++) {
+            if (field[i][k]) {
+                std::cout << "Pop! ";
+                field[i][k] = false;
+            }
+        }
+    }
+    std::cout << std::endl;
+}
+
 int main() {
     // 14.6.2
+    /*
     char field[3][3];
     for (int i = 0; i < 3; i++) {
         for (int k = 0; k < 3; k++) {
@@ -56,5 +104,28 @@ int main() {
         }
         counter++;
     }
+    */
+
+    // 14.6.5
+    /*
+    bool field[12][12];
+    initField(field);
+    
+    int x1, y1, x2, y2;
+    while(!fieldPopped(field)) {
+        bool correctInput = false;
+        while(!correctInput) {
+                std::cin >> x1 >> y1 >> x2 >> y2;
+                if (x1 > -1 && x1 < 12 && y1 > -1 && y1 < 12 && x2 > -1 && x2 < 12 && y2 > -1 && y2 < 12) {
+                    correctInput = true;
+                    popping(field, x1, y1, x2, y2);
+                } else {
+                    std::cout << "Incorrect input!\n";
+                }
+        }
+        showField(field);
+    }
+    std::cout << "GameOver!" << std::endl;
+    */
 
 }
